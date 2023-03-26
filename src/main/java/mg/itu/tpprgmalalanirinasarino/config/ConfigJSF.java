e659363e17a4a333f2cc06b55a8f06cdd3b0084d
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2023-03-26 15:03:23 +0300

    Etat initial

---
+++ src/main/java/mg/itu/tpprgmalalanirinasarino/config/ConfigJSF.java
@@ -0,0 +1,20 @@
+package mg.itu.tpprgmalalanirinasarino.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sarino
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
