bd2b5f263d9af5777e5c1771a080511c84df9320
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2023-03-26 21:27:13 +0300

    Le nombre précédemment saisi apparaisse par défaut dans le formulaire lorsqu'on veut saisir un autre nombre

---
+++ src/main/java/jsf/bean2.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package jsf;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Sarino
+ */
+@Named(value = "bean2")
+@RequestScoped
+public class bean2 {
+
+    private int nombre;
+
+    @PostConstruct
+    public void init() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Map<String, String> requestParams = facesContext.getExternalContext().getRequestParameterMap();
+        String nombreString = requestParams.get("nb");
+        if (nombreString != null) {
+            nombre = Integer.parseInt(nombreString);
+        } else {
+            nombre = 0;
+        }
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    public String afficher() {
+        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+}
